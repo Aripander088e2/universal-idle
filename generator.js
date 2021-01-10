5c8c8df86551339cf22b086e9276756737e3949a
Glitch (hello-webpage) <none>
2021-01-10 08:00:44 +0000

    🐪🚎 Checkpoint ./generator.js:10435989/392

--- generator.js
+++ generator.js
@@ -18,18 +18,20 @@ function getGeneratorMulti(gen){
 
 function getGeneratorExp(gen){
   let base = new Decimal(1)
-  if (game.atoms.gte(1e80)) base = base.div(game.atoms.log10().div(80).pow(0.5))
+  if (game.atoms.gte(1e80)) base = base.div(game.atoms.log10().div(80).pow(1))
   return base
 }
 
 function getSizeSpeed(){
   let base = (game.atoms.gte(1024) && game.generator[2].gt(0) ? game.atoms.pow(0.3).div(8) : new Decimal(0))
-  if (game.size.gte(4.4e26)) base = base.pow(new Decimal(1).div(game.size.log10().div(new Decimal(4.4).log10()).add(26)).pow(0.5))
+  base = base.pow(getSizeSpeedExp())
   return base
 }
 
 function getSizeSpeedExp(){
-  
+  let base = new Decimal(1)
+  if (game.size.gte(4.4e26)) base = base.div(game.size.log10().div(new Decimal(4.4).log10().add(26)).pow(1))
+  return base
 }
 
 function getSizeBoost(){
