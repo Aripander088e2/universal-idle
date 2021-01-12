e0de26420db53839965cb2af38c8c39f5e2d928f
Glitch (hello-webpage) <none>
2021-01-12 03:48:02 +0000

    ⛩😋 Checkpoint ./index.html:10435989/105 ./tab.js:10435989/62

--- tab.js
+++ tab.js
@@ -1,6 +1,7 @@
 function Tab(t) {
   document.getElementById("tab1").style.display = "none";
   document.getElementById("tab101").style.display = "none";
+  document.getElementById("tab102").style.display = "none";
   document.getElementById("tab" + t).style.display = "block"
 }
 
