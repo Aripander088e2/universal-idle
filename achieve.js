3c1dbebad2a61671208d7ab2e5eb0f6761572390
Glitch (hello-webpage) <none>
2021-01-12 07:52:18 +0000

    🏭🐟 Checkpoint ./generator.js:10435989/76 ./achieve.js:10435989/99

--- achieve.js
+++ achieve.js
@@ -22,7 +22,7 @@ const achreward =
         "", 
         "", 
         "", 
-        ""
+        ", Reward: Your atoms explain your universe size"
        ]
       ]
 
--- generator.js
+++ generator.js
@@ -29,7 +29,7 @@ function getGeneratorSpeedExp(gen){
 }
 
 function getSizeSpeed(){
-  let base = (game.atoms.gte(1e36) && game.generator[8].gt(0) ? game.atoms.pow(0.5).div(1e18) : new Decimal(0))
+  let base = (game.achievement.includes(18) ? game.atoms.pow(0.5).div(1e18) : new Decimal(0))
   if (base.gte(1e24)) base = new Decimal(10).pow(base.log10().mul(24).pow(0.5))
   return base
 }
