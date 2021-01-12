07b075118dc017687a1a1769a8e0e8ad99c3c82e
Glitch (hello-webpage) <none>
2021-01-12 06:55:23 +0000

    😋🚩 Checkpoint ./achieve.js:10435989/910 ./index.html:10435989/34

--- achieve.js
+++ achieve.js
@@ -1,5 +1,23 @@
+const achdesc = 
+      [null, 
+       [null, 
+        "You gotta start somewhere: Buy a Generator I", 
+        "1000 atoms is a lot: Buy a Generator II", 
+        "Half life 3 CONFIRMED: Buy a Generator III", 
+        "L4G: Left 4 Generators: Buy a Generator IV", 
+        "	5 Generators Atom Punch", 
+        "", 
+        "", 
+        ""
+       ]
+      ]
+
 function updateAchievement(){
-  document.getElementById("ach11").innerHTML = ""
+  for (let i = 1; i < 1.5; i++){
+    for (let j = 1; j < 8.5; j++){
+      document.getElementById("ach" + i + j).innerHTML = achdesc[i][j]
+    }
+  }
 }
 
 function getAchievement(){
